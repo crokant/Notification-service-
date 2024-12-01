62618ee272b6f782b9020a76df84fa47894b9097
Ilya Safronov <yaizkima@gmail.com>
2024-12-01 14:48:03 +0300

    interface update

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
+function Home() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(`${apiUrl}/api/hello`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>Error: {error}</p>
+            ) : (
+                data && (
+                    <div>
+                        <p>Message: {data.message}</p>
+                        <p>Origin: {data.origin}</p>
+                    </div>
+                )
+            )}
+        </div>
+    );
+}
+
+export default Home;
