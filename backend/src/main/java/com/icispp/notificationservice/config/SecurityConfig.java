f8aeba861e62108d1d308133ea182ed307eb5abe
crokant <zavoronkovviktor87@gmail.com>
2024-12-02 16:10:05 +0300

    .

---
+++ backend/src/main/java/com/icispp/notificationservice/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.icispp.notificationservice.config;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/register/**", "/login/**").permitAll()
+                .antMatchers("/manager/**").hasAuthority("MANAGER")
+                .antMatchers("/user/**").hasAuthority("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
