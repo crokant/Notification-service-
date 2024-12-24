76020a871ef7b1baa8c5dd02f97684deee6b32f5
andrey <ronorothog2003@gmail.com>
2024-12-24 13:59:09 +0300

    my contribution

--- backend/src/main/java/com/icispp/notificationservice/services/MessageService.java
+++ backend/src/main/java/com/icispp/notificationservice/services/MessageService.java
@@ -1,10 +1,12 @@
 package com.icispp.notificationservice.services;
 
+import com.icispp.notificationservice.Entity.EmailDetails;
 import com.icispp.notificationservice.models.Message;
 import com.icispp.notificationservice.models.Subscription;
 import com.icispp.notificationservice.models.User;
 import com.icispp.notificationservice.repositories.MessageRepository;
 import com.icispp.notificationservice.repositories.SubscriptionRepository;
+import com.icispp.notificationservice.services.EmailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,13 @@ import java.time.LocalDateTime;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final EmailServiceImpl emailService;
 
 
     @Autowired
-    public MessageService(MessageRepository messageRepository, SubscriptionRepository subscriptionRepository) {
+    public MessageService(MessageRepository messageRepository, SubscriptionRepository subscriptionRepository, EmailServiceImpl emailService) {
         this.messageRepository = messageRepository;
+        this.emailService = emailService;
     }
 
     public Message sendMessageToUser(String subject, String content, User user, Subscription subscription) {
@@ -30,6 +34,9 @@ public class MessageService {
                 .sentAt(LocalDateTime.now())
                 .delivered(false)
                 .build();
+
+        emailService.sendSimpleMailToUser(message, user);
+
         return messageRepository.save(message);
     }
 
